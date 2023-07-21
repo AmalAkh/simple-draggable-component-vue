bb8cd813fd99639585ef9c6e4d8dafa32a86f698
AmalAkh <amal.akhmadinurov@gmail.com>
2023-07-21 21:51:09 +0200

    CSS example updated

--- demo/src/components/CssExample.vue
+++ demo/src/components/CssExample.vue
@@ -30,6 +30,7 @@ function cloneToGroup2(item)
 
 </script>
 <template>
+    
     <div class="container">
         <div class="column">
             <DraggableElement  :clone="cloneToGroup1"  class="group-1-dragable" item-key="id" v-model="group1.items">
@@ -51,12 +52,43 @@ function cloneToGroup2(item)
             </DraggableElement >
         </div>
     </div>
+    <div>
+        <h3>CSS code</h3>
+        .sdcv-dragging<br>
+        {<br>
+            &nbsp;.drag-item<br>
+            &nbsp;{<br>
+                &nbsp;&nbsp;background-color: red;<br>
+                &nbsp;}<br>
+        }<br>
+        .sdcv-moving<br>
+        {<br>
+            &nbsp;.drag-item<br>
+            &nbsp;{
+                &nbsp;&nbsp;background-color: yellow;<br>
+                &nbsp; }<br>
+        }<br>
+    </div>
 </template>
 <style lang="scss" scoped>
     .container
     {
         display: flex;
         
+    }
+    .sdcv-dragging
+    {
+        .drag-item
+        {
+            background-color: red;
+        }
+    }
+    .sdcv-moving
+    {
+        .drag-item
+        {
+            background-color: yellow;
+        }
     }
     .column
     {
